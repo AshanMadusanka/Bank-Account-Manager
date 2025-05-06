2a5c2a2e0d76d135c1c1baa174595c59a89ceaec
Ashan <2017t00036@stu.cmb.ac.lk>
2025-05-06 14:28:35 +0530

    Create Functions

--- main.c
+++ main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdint.h>
+
+uint32_t acc_count;
+
+typedef struct {
+
+    char name[50];
+    uint32_t account_number;
+    float balance;
+}Account_Data_t;
+
+Account_Data_t account_data[50];
+
+void showAllAccount(Account_Data_t *account_data, uint32_t *acc_count);
+void withdrawMoney(Account_Data_t *account_data, uint32_t *acc_count);
+void depositMoney(Account_Data_t *account_data, uint32_t *acc_count);
+void createNewAccount(Account_Data_t *account_data, uint32_t *acc_count);
 
 int main(void) {
     printf("Hello, World!\n");
     return 0;
+}
+
+
+
+void createNewAccount(Account_Data_t *account_data, uint32_t *acc_count) {
+
+
+
+}
+
+void depositMoney(Account_Data_t *account_data, uint32_t *acc_count) {
+
+
+
+}
+
+
+void withdrawMoney(Account_Data_t *account_data, uint32_t *acc_count) {
+
+
+
+}
+
+void showAllAccount(Account_Data_t *account_data, uint32_t *acc_count) {
+
+
+
 }
